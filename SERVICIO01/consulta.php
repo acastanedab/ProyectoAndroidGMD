62aca89eb6882802078b1efcc24cd74fd5c779f3
acastanedab <goldragon30@gmail.com>
2017-02-14 22:07:03 -0500

    Add files via upload

---
+++ SERVICIO01/consulta.php
@@ -0,0 +1,14 @@
+<?php
+include('functions.php');
+$ART_COD=$_GET["ART_COD"];
+
+
+if($resultset=getSQLResultSet("SELECT SEG_COD,SEG_FEC,SEG_COR FROM `TB_SEGUIMIENTO` WHERE SEG_ART_COD='$ART_COD'")){
+	while ($row = $resultset->fetch_array(MYSQLI_NUM)){
+		echo json_encode($row);
+	}
+}
+
+?>
+
+
---
+++ SERVICIO01/functions.php
@@ -0,0 +1,36 @@
+<?php 
+header( 'Content-Type: text/html;charset=utf-8' );
+
+function ejecutarSQLCommand($commando){ 
+  $mysqli = new mysqli("localhost", "root", "mysql", "bd_proyecto");
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+if ( $mysqli->multi_query($commando)) {
+     if ($resultset = $mysqli->store_result()) {
+    	while ($row = $resultset->fetch_array(MYSQLI_BOTH)) {    		
+    	}
+    	$resultset->free();
+     }  
+}
+
+$mysqli->close();
+}
+
+function getSQLResultSet($commando){ 
+  $mysqli = new mysqli("localhost", "root", "mysql", "bd_proyecto");
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+if ( $mysqli->multi_query($commando)) {
+	return $mysqli->store_result();   
+}
+$mysqli->close();
+}
+
+
+?>
