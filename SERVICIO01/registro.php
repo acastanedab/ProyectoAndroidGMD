9cb3e2b45ad849d4b63ecd764e8bd48296c6b1a1
acastanedab <goldragon30@gmail.com>
2017-02-14 22:11:11 -0500

    Delete registro.php

--- SERVICIO01/registro.php
+++
@@ -1,8 +0,0 @@
-<?php include ('functions.php');
-$ART_COD=$_GET['ART_COD'];
-$SEG_FEC=date("Y-m-d H:i:s");
-$SEG_COR=$_GET['SEG_COR'];
-
-ejecutarSQLCommand("INSERT INTO `TB_SEGUIMIENTO` (SEG_ART_COD,SEG_FEC,SEG_COR) VALUES ('$ART_COD','$SEG_FEC','$SEG_COR');");
-
- ?>
