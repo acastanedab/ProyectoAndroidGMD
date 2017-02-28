d2f1c9d40c81bd5fd4852fe487bae588c7aca169
acastanedab <goldragon30@gmail.com>
2017-02-28 02:25:25 -0500

    usando retrofit

---
+++ MyApplicationGPS/app/src/main/java/com/example/angeleloy/gps/Adapter/PedidoAdapter.java
@@ -0,0 +1,73 @@
+package com.example.angeleloy.gps.Adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.angeleloy.gps.Entidades.PedidoBE;
+import com.example.angeleloy.gps.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by AngelEloy on 28/02/2017.
+ */
+
+public class PedidoAdapter extends BaseAdapter {
+
+    List<PedidoBE> listaPreferencia;
+    static class ViewHolder{
+        TextView textViewCodigoArticulo;
+        TextView textViewNombreArticulo;
+        TextView textViewDescripcionArticulo;
+        TextView textViewPrecioArticulo;
+    }
+
+    private LayoutInflater inflater;
+
+    public PedidoAdapter(Context context, List<PedidoBE> listaPreferencia) {
+        inflater = LayoutInflater.from(context);
+        this.listaPreferencia = listaPreferencia;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+    @Override
+    public int getCount() {
+        return listaPreferencia.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.pedido_item,null);
+
+            holder = new ViewHolder();
+            holder.textViewCodigoArticulo = (TextView)convertView.findViewById(R.id.txtCodigoArticulo);
+            holder.textViewNombreArticulo = (TextView)convertView.findViewById(R.id.txtNombreArticulo);
+            holder.textViewDescripcionArticulo = (TextView)convertView.findViewById(R.id.txtDescripcionArticulo);
+            holder.textViewPrecioArticulo = (TextView)convertView.findViewById(R.id.txtPrecioArticulo);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder)convertView.getTag();
+        }
+        holder.textViewCodigoArticulo.setText(listaPreferencia.get(position).getCodigoArticulo());
+        holder.textViewNombreArticulo.setText(listaPreferencia.get(position).getNombreArticulo());
+        holder.textViewDescripcionArticulo.setText(listaPreferencia.get(position).getDescripcionArticulo());
+        holder.textViewPrecioArticulo.setText(listaPreferencia.get(position).getPrecioArticulo().toString());
+
+        return convertView;
+    }
+}
---
+++ MyApplicationGPS/app/src/main/java/com/example/angeleloy/gps/Servicios/PedidoService.java
@@ -0,0 +1,23 @@
+package com.example.angeleloy.gps.Servicios;
+
+import com.example.angeleloy.gps.Entidades.PedidoBE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by AngelEloy on 28/02/2017.
+ */
+
+public interface PedidoService {
+    @GET("articulo/listar")
+    void getPedidos(Callback<List<PedidoBE>> callback);
+
+    @GET("articulo/listar")
+    List<PedidoBE> getPedidos();
+}
